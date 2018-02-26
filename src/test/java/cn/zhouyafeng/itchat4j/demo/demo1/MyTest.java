6b613a547a0ca494e642dcf31b170925e1acf64f
wuhaisheng <wuhaisheng@binggou.com>
2018-02-26 15:20:39 +0800

    测试

--- src/test/java/cn/zhouyafeng/itchat4j/demo/demo1/MyTest.java
+++ src/test/java/cn/zhouyafeng/itchat4j/demo/demo1/MyTest.java
@@ -1,7 +1,13 @@
 package cn.zhouyafeng.itchat4j.demo.demo1;
 
 import cn.zhouyafeng.itchat4j.Wechat;
+import cn.zhouyafeng.itchat4j.api.WechatTools;
 import cn.zhouyafeng.itchat4j.face.IMsgHandlerFace;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  *
@@ -11,10 +17,19 @@ import cn.zhouyafeng.itchat4j.face.IMsgHandlerFace;
  *
  */
 public class MyTest {
+
+	private static final Logger LOG = LoggerFactory.getLogger(MyTest.class);
+
 	public static void main(String[] args) {
 		String qrPath = "D://itchat4j//login"; // 保存登陆二维码图片的路径，这里需要在本地新建目录
 		IMsgHandlerFace msgHandler = new SimpleDemo(); // 实现IMsgHandlerFace接口的类
 		Wechat wechat = new Wechat(msgHandler, qrPath); // 【注入】
 		wechat.start(); // 启动服务，会在qrPath下生成一张二维码图片，扫描即可登陆，注意，二维码图片如果超过一定时间未扫描会过期，过期时会自动更新，所以你可能需要重新打开图片
+		LOG.info("+++++++++++++++++++获取好友数量+++++++++++++++++++++");
+		List<JSONObject> contactList = WechatTools.getContactList();
+		LOG.info("好友数量"+contactList.size());
+
+
+
 	}
 }
