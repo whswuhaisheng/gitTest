17606fae2a8744aa014b4066b311b4c716a0d121
wuhaisheng <wuhaisheng@binggou.com>
2018-03-02 09:57:35 +0800

    测试

--- src/test/java/cn/zhouyafeng/itchat4j/demo/demo1/MyTest.java
+++ src/test/java/cn/zhouyafeng/itchat4j/demo/demo1/MyTest.java
@@ -2,7 +2,6 @@ package cn.zhouyafeng.itchat4j.demo.demo1;
 
 import cn.zhouyafeng.itchat4j.Wechat;
 import cn.zhouyafeng.itchat4j.api.WechatTools;
-import cn.zhouyafeng.itchat4j.face.IMsgHandlerFace;
 import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,15 +20,15 @@ public class MyTest {
 	private static final Logger LOG = LoggerFactory.getLogger(MyTest.class);
 
 	public static void main(String[] args) {
-		String qrPath = "D://itchat4j//login"; // 保存登陆二维码图片的路径，这里需要在本地新建目录
-		IMsgHandlerFace msgHandler = new SimpleDemo(); // 实现IMsgHandlerFace接口的类
-		Wechat wechat = new Wechat(msgHandler, qrPath); // 【注入】
-		wechat.start(); // 启动服务，会在qrPath下生成一张二维码图片，扫描即可登陆，注意，二维码图片如果超过一定时间未扫描会过期，过期时会自动更新，所以你可能需要重新打开图片
+		String qrPath = "D://itchat4j//login"; // 保存登陆二维码图片的路径
+		new Wechat(new SimpleDemo(), qrPath).start();
 		LOG.info("+++++++++++++++++++获取好友数量+++++++++++++++++++++");
 		List<JSONObject> contactList = WechatTools.getContactList();
+		String msg ="张灯结彩月儿圆，又到元宵佳节时;花灯俏俏惹人爱，汤圆香香诱人馋;缤纷烟花洒吉祥，" +
+				"喜笑颜开福无边;送份祝福表心意，情浓谊厚心舒畅;慕小谦祝你元宵节开心，幸福快乐爽歪歪!";//元宵节祝福文案
 		LOG.info("好友数量"+contactList.size());
-
-
-
+		for (JSONObject jsonObject:contactList) {
+			WechatTools.sendMsgByUserName(msg,jsonObject.getString("UserName"));
+		}
 	}
 }
